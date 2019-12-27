780763f3ca85b9dbea6d94b15bd3198659fa25ac
Idelfonso Gutierrez <idelfonsog2@gmail.com>
2019-12-26 21:49:32 -0500

    add models

---
+++ Sources/AppModels/InfoData.swift
@@ -0,0 +1,12 @@
+//
+//  InfoData.swift
+//  App
+//
+//  Created by idelfonso Gutierrez on 12/26/19.
+//
+
+import Vapor
+
+struct InfoData: Content {
+    let name: String
+}
---
+++ Sources/AppModels/InfoResponse.swift
@@ -0,0 +1,12 @@
+//
+//  InfoResponse.swift
+//  App
+//
+//  Created by idelfonso Gutierrez on 12/26/19.
+//
+
+import Vapor
+
+struct InfoResponse: Content {
+    let request: InfoData
+}
