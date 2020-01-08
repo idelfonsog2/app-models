e8455811300fbfcc632347df231d05efa78d7767
Idelfonso Gutierrez <idelfonsog2@gmail.com>
2020-01-07 21:59:53 -0600

    wip: device token model

--- Sources/AppModels/DeviceToken.swift
+++ Sources/AppModels/DeviceToken.swift
@@ -5,20 +5,23 @@
 //  Created by idelfonso Gutierrez on 12/31/19.
 //
 
-import Fluent
+import FluentPostgreSQL
 import Vapor
 import Foundation
 
 public final class DeviceToken: Codable {
     public var id: Int?
     public var token: Data
-    public var platform: String
     
     public init(id: Int? = nil, token: Data, platform: String) {
         self.id = id
         self.token = token
-        self.platform = platform
     }
 }
 
 extension DeviceToken: Content { }
+extension DeviceToken: Migration { }
+extension DeviceToken: Parameter { }
+
+extension DeviceToken: PostgreSQLModel { }
+
