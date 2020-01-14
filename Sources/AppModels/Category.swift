b019aaac3363ab054451f47f3ac29914b44bbd5b
Idelfonso Gutierrez <idelfonsog2@gmail.com>
2020-01-13 21:14:28 -0600

    codable for category

--- Sources/AppModels/Category.swift
+++ Sources/AppModels/Category.swift
@@ -6,16 +6,15 @@
 //
 
 import Foundation
-
 import Vapor
 import FluentPostgreSQL
 
 /// design to stablish Many to many relationship
 public final class Category: Codable {
-    var id: Int?
-    var name: String?
+    public var id: Int?
+    public var name: String?
     
-    init(name: String) {
+    public init(name: String) {
         self.name = name
     }
 }
