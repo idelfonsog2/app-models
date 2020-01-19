c75a45fed40c677c8e7e70699788c6d0353850e2
Idelfonso Gutierrez <idelfonsog2@gmail.com>
2020-01-19 16:45:48 -0600

    fix: TodoCategoryPivot model [ci skip]

--- Sources/AppModels/UserAirportPivot.swift
+++ Sources/AppModels/UserAirportPivot.swift
@@ -8,7 +8,7 @@
 import FluentPostgreSQL
 import Foundation
 
-final class TodoCategoryPivot: PostgreSQLUUIDPivot {
+final class TodoCategoryPivot: PostgreSQLUUIDPivot {    
     var id: UUID?
     
     var todoID: Todo.ID
@@ -17,14 +17,14 @@ final class TodoCategoryPivot: PostgreSQLUUIDPivot {
     typealias Left = Todo
     typealias Right = Categoria
     
-    static let leftIDKey: \.todoID
-    static let rigthIDKey: \.categoriaID
+    static let leftIDKey: LeftIDKey = \.todoID
+    static let rightIDKey: RightIDKey = \.categoryID
     
-    init(_ todo: Todo, _ category: Categoria) {
+    init(_ todo: Todo, _ category: Categoria) throws {
         self.todoID = try todo.requireID()
         self.categoryID = try category.requireID()
     }
 }
 
 extension TodoCategoryPivot: Migration { }
-extension TodoCategoryPivot: ModifiablePivot
+extension TodoCategoryPivot: ModifiablePivot { }
