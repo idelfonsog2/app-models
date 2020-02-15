1787fb72558e6e5bebf28ca3cf22793326583d71
Idelfonso Gutierrez <idelfonsog2@gmail.com>
2020-02-15 10:37:43 -0600

    update dependencies of dependencies

--- Package.swift
+++ Package.swift
@@ -13,14 +13,13 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.2"),
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "AppModels",dependencies: ["FluentSQLite", "Vapor", "FluentPostgreSQL"]),
+        .target(name: "AppModels",dependencies: ["Vapor", "FluentPostgreSQL"]),
         .testTarget(name: "AppModelsTests",dependencies: ["AppModels"]),
     ]
 )
